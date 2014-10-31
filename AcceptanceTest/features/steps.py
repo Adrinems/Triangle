08de26dbfd5218cb8e4bb60e27072ed16c7f332e
Adrinems <adrianasorianom@gmail.com>
2014-10-31 12:34:12 -0600

    Third Commit

--- AcceptanceTest/features/steps.py
+++ AcceptanceTest/features/steps.py
@@ -5,17 +5,19 @@ import sys
 sys.path.append('../')
 from Triangle import Triangle
 
+
 @step(u'Given: Un triangulo de lados "([^"]*)", "([^"]*)", "([^"]*)"')
 def given_un_triangulo_de_lados_group1_group2_group1(step, a, b, c):
-    world.parametros=[int(a), int(b), int(c)]
+    world.parametros = [int(a), int(b), int(c)]
+
 
 @step(u'When: realizo el calculo')
 def when_realizo_el_calculo(step):
-	trian = Triangle(world.parametros)
-	world.tipo = trian.istriangle()
-   
+    trian = Triangle(world.parametros)
+    world.tipo = trian.istriangle()
+
 
 @step(u'Then: obtengo un triangulo de tipo "([^"]*)"')
 def then_obtengo_un_triangulo_de_tipo_group1(step, tipo):
-	assert world.tipo == tipo, "resultado obtenido {0}, resultado esperado {1}".format(world.tipo, tipo)
-    
+    assert world.tipo == tipo, "resultado obtenido {0}, resultado esperado {1}".format(
+        world.tipo, tipo)
--- Triangle.py
+++ Triangle.py
@@ -1,20 +1,20 @@
 class Triangle:
 
-	def __init__(self,args):
-		self.lado= args[0:]
+    def __init__(self, args):
+        self.lado = args[0:]
 
-	def istriangle(self):
-		a=self.lado[0]
-		b=self.lado[1]
-		c=self.lado[2]
-		
-		if(a >200 or b > 200 or c > 200):
-			return "NotATriangle"
-		elif a > (b+c) or b > (a+c) or c > (a+b):
-			return "NotATriangle"
-		elif a == b and b == c:
-			return "Equilateral"
-		elif a!=b and b!=c and a!=c:
-			return "Scalene"
-		else:
-			return "Isosceles"
+    def istriangle(self):
+        a = self.lado[0]
+        b = self.lado[1]
+        c = self.lado[2]
+
+        if(a > 200 or b > 200 or c > 200):
+            return "NotATriangle"
+        elif a > (b + c) or b > (a + c) or c > (a + b):
+            return "NotATriangle"
+        elif a == b and b == c:
+            return "Equilateral"
+        elif a != b and b != c and a != c:
+            return "Scalene"
+        else:
+            return "Isosceles"
